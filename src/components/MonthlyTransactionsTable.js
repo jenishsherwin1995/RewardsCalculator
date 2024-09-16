c2af807b8e384f254cebaaf2fb1f934ae4da9fa7
jenishsherwin1995 <sherwinjenish.e@gmail.com>
2024-09-16 10:06:50 +0530

    added total points at bottom row

--- src/components/MonthlyTransactionsTable.js
+++ src/components/MonthlyTransactionsTable.js
@@ -12,44 +12,58 @@ import {
 
 const MonthlyTransactionsTable = ({ monthlyData }) => {
   return (
-    Object.entries(monthlyData).map(([monthKey, transactions]) => (
-      <TableContainer component={Paper} style={{ marginBottom: '30px' }} key={monthKey}>
-        <Typography variant="h6" gutterBottom align="center">
-          Transactions for {monthKey}
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ fontWeight: 'bold' }}>Customer Name</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Customer ID</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Transaction ID</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Year</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Month</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Spent Amount</TableCell>
-              <TableCell style={{ fontWeight: 'bold' }}>Points</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {transactions.map((transaction) => (
-              <TableRow key={transaction.transactionId}>
-                <TableCell>{transaction.customer}</TableCell>
-                <TableCell>{transaction.customerId}</TableCell>
-                <TableCell>{transaction.transactionId}</TableCell>
-                <TableCell>{transaction.year}</TableCell>
-                <TableCell>{transaction.month}</TableCell>
-                <TableCell>${transaction.amount.toFixed(2)}</TableCell>
-                <TableCell>{transaction.points.toFixed(2)}</TableCell>
-              </TableRow>
-            ))}
-            {transactions.length === 0 && (
+    Object.entries(monthlyData).map(([monthKey, transactions]) => {      
+      const totalPoints = transactions.reduce((sum, transaction) => sum + (transaction.points || 0), 0);
+
+      return (
+        <TableContainer component={Paper} style={{ marginBottom: '30px' }} key={monthKey}>
+          <Typography variant="h6" gutterBottom align="center">
+            Transactions for {monthKey}
+          </Typography>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan={7} align="center">No transactions</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Customer Name</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Customer ID</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Transaction ID</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Year</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Month</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Spent Amount</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Points</TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    ))
+            </TableHead>
+            <TableBody>
+              {transactions.map((transaction) => (
+                <TableRow key={transaction.transactionId}>
+                  <TableCell>{transaction.customer}</TableCell>
+                  <TableCell>{transaction.customerId}</TableCell>
+                  <TableCell>{transaction.transactionId}</TableCell>
+                  <TableCell>{transaction.year}</TableCell>
+                  <TableCell>{transaction.month}</TableCell>
+                  <TableCell>${transaction.amount.toFixed(2)}</TableCell>
+                  <TableCell>{transaction.points.toFixed(2)}</TableCell>
+                </TableRow>
+              ))}
+              
+              <TableRow>
+                <TableCell colSpan={6} align="right" style={{ fontWeight: 'bold' }}>
+                  Total Earned Points:
+                </TableCell>
+                <TableCell align="center" style={{ fontWeight: 'bold' }}>
+                  {totalPoints.toFixed(2)}
+                </TableCell>
+              </TableRow>
+
+              {transactions.length === 0 && (
+                <TableRow>
+                  <TableCell colSpan={7} align="center">No transactions</TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    })
   );
 };
 
