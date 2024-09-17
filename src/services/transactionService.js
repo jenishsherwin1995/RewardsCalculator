5ea73f618b5ba7d32b9b9e0fe9c992b38295797c
jenishsherwin1995 <sherwinjenish.e@gmail.com>
2024-09-17 18:08:55 +0530

    removed logger

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,18 +1,17 @@
 import { constants } from "../utils/Constants";
-import logger from '../logger';
 
 export const getTransactionsData = async () => {
   try {
     const response = await fetch("/customerTransactionsData.json");
-    if (!response.ok) {
-      logger.error(constants.ERROR_MESSAGE);
+    if (!response.ok) {      
       throw new Error(constants.ERROR_MESSAGE);
     }
-    const data = await response.json();
-    logger.log('Fetch Customer Transaction Data', data);
+    const data = await response.json();    
     return data;
   } catch (err) {
-    logger.error(constants.ERROR_MESSAGE, err);
+   // console.error(err);
     throw err;
+  } finally {
+    // console.log(fetch success);
   }
 };
